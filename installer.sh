5d0ff5e3a184af7175a2cf50017ba9dab1a6c80d
markson <markson@2333ma.top>
2023-05-27 14:05:14 +0800

    Add auto install curl and unzip

--- installer.sh
+++ installer.sh
@@ -9,22 +9,32 @@ home_path="$(pwd)"
 
 download_installer(){
     echo "$GREEN""We will download the installer twice for checking sha256""$RESET"
-    curl -L https://github.com/MarksonHon/arch-install-scripts/archive/refs/heads/main.zip -o arch-installer.zip
-    curl -L https://github.com/MarksonHon/arch-install-scripts/archive/refs/heads/main.zip -o arch-installer.zip.2
+    curl -L https://github.com/MarksonHon/arch-installer/archive/refs/heads/master.zip -o arch-installer.zip
+    curl -L https://github.com/MarksonHon/arch-installer/archive/refs/heads/master.zip -o arch-installer.zip.2
     sha256_download_once="$(sha256sum ./arch-installer.zip)"
     sha256_download_twice="$(sha256sum ./arch-installer.zip.2)"
     if [ "$sha256_download_once" != "$sha256_download_twice" ];then
         echo "$RED""Failed to pass sha256 check, please try again!""$RESET"
         return 1
     fi
-    unzip arch-installer.zip; rm arch-installer.zip*
+    unzip arch-installer.zip; rm arch-installer.zip*; chmod +x ./arch-installer/*sh; chmod +x ./arch-installer/bin/*sh
 }
 
-echo "$YELLOW""The path of installer is $home_path""$RESET"
+for tool in curl unzip; do
+    if ! command -v $tool> /dev/null 2>&1; then
+        tool_need="$tool"" ""$tool_need"
+    fi
+done
+if ! pacman -Sy "$tool_need";then
+        echo "$RED""Use system package manager to install $tool_need failed,""$RESET"
+        echo "$RED""You should install $tool_need then try again.""$RESET"
+        exit 1
+fi
 
 if ! download_installer;then
     echo "$RED""Failed to download installer! Check your network and try again!""$RESET"
-    exit
+    exit 1
 fi
 
+echo "$YELLOW""The path of installer is $home_path""$RESET"
 ./arch-installer/bin/setup-arch.sh
