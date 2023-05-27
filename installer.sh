50703df9aecdbf25a9d12eba5e5a238257c0d71f
markson <markson@2333ma.top>
2023-05-27 16:02:47 +0800

    Use git clone

--- installer.sh
+++ installer.sh
@@ -7,19 +7,6 @@ RESET=$(tput sgr0)
 
 home_path="$(pwd)"
 
-download_installer(){
-    echo "$GREEN""We will download the installer twice for checking sha256...""$RESET"
-    curl -L https://github.com/MarksonHon/arch-installer/archive/refs/heads/master.zip -o arch-installer.zip
-    curl -L https://github.com/MarksonHon/arch-installer/archive/refs/heads/master.zip -o arch-installer.zip.2
-    sha256_download_once="$(sha256sum ./arch-installer.zip | awk -F ' ' '{print $1}')"
-    sha256_download_twice="$(sha256sum ./arch-installer.zip.2 | awk -F ' ' '{print $1}')"
-    if [ "$sha256_download_once" != "$sha256_download_twice" ];then
-        echo "$RED""Failed to pass sha256 check, please try again!""$RESET"
-        return 1
-    fi
-    unzip arch-installer.zip; rm arch-installer.zip*; chmod +x ./arch-installer/*sh; chmod +x ./arch-installer/bin/*sh
-}
-
 for tool in curl unzip git; do
     if ! command -v $tool> /dev/null 2>&1; then
         tool_need="$tool"" ""$tool_need"
@@ -31,10 +18,8 @@ if ! /bin/bash -c "pacman -Sy $tool_need";then
         exit 1
 fi
 
-if ! download_installer;then
-    echo "$RED""Failed to download installer! Check your network and try again!""$RESET"
-    exit 1
-fi
+git clone https://github.com/MarksonHon/arch-installer/
 
 echo "$YELLOW""The path of installer is $home_path""$RESET"
+
 ./arch-installer/bin/setup-arch.sh
