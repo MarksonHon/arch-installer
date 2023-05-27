a9ef29cb48d21718bf98d9fa4b30d922a174394f
markson <markson@2333ma.top>
2023-05-27 17:59:40 +0800

    Fix install systemd-boot

--- bin/install-systemd-boot.sh
+++ bin/install-systemd-boot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-arch-chroot /mnt /bin/bash -c "bootctl install --esp-path=$ESP"
+arch-chroot /mnt /bin/bash -c "bootctl install --esp-path=/efi"
 root_partition_part_uuid=$(lsblk "$ROOT_PARTITION" -no PARTUUID)
 echo "root=PARTUUID=$root_partition_part_uuid rw rootfstype=ext4" > /mnt/etc/kernel/cmdline
 mkdir -p /mnt/etc/mkinitcpio.d/
-cat "./customs/systemd-boot-uki-kernel.conf" | sed "s|ESP|$ESP|g" | sed "s|linux|$kernel_to_install|g" | tee "/mnt/etc/mkinitcpio.d/""$kernel_to_install"".preset.pacsave"
+cat "./customs/systemd-boot-uki-kernel.conf" | sed "s|linux|$kernel_to_install|g" | tee "/mnt/etc/mkinitcpio.d/""$kernel_to_install"".preset.pacsave"
 pacstrap /mnt mkinitcpio "$kernel_to_install" "$kernel_to_install""-headers"
