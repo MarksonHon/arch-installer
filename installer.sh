9c46999d394ebbefc9584fc1b14ceffe1b7524cf
markson <markson@2333ma.top>
2023-06-08 14:00:33 +0800

    maybe can work

--- installer.sh
+++ installer.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+RESET=$(tput sgr0)
+
 for tool in curl unzip git; do
     if ! command -v $tool> /dev/null 2>&1; then
         tool_need="$tool"" ""$tool_need"
     fi
 done
 /bin/bash -c "pacman -Sy $tool_need"
+
+echo "$GREEN""-----------------------------------------------------------""$RESET"
+echo "$GREEN""-----------------------------------------------------------""$RESET"
+echo "Now you can install the Arch Linux!"
+echo "Before installing, make sure you have the partition OK with fdisk or cfdisk,"
+echo "and you have backup files before installing."
+echo "$GREEN""-----------------------------------------------------------""$RESET"
+echo "$GREEN""-----------------------------------------------------------""$RESET"
 
 git clone https://github.com/MarksonHon/arch-installer/
 
