d7c0bc4d49b046cf3b938517c67d2ac4b79a6e71
Markson Hon <50002150+MarksonHon@users.noreply.github.com>
2023-06-12 03:42:55 +0000

    notice read official docs

--- installer.sh
+++ installer.sh
@@ -10,16 +10,25 @@ for tool in curl unzip git; do
         tool_need="$tool"" ""$tool_need"
     fi
 done
-if ! (/bin/bash -c "pacman -Sy $tool_need");then
-    echo "$RED""ERROR: Failed to install $tool_need, check your network settings or permission then try again!""$RESET"
-    exit 1
+if [ -n "$tool_need" ]; then
+    if ! (/bin/bash -c "pacman -Sy $tool_need");then
+        echo "$RED""ERROR: Failed to install $tool_need, check your network settings or permission then try again!""$RESET"
+        exit 1
+    fi
 fi
-
 echo "$GREEN""-----------------------------------------------------------------------------""$RESET"
 echo "$GREEN""-----------------------------------------------------------------------------""$RESET"
 echo "Now you can install the Arch Linux!"
 echo "Before installing, make sure you have the partition OK with fdisk or cfdisk,"
-echo "and you have backup files before installing."
+echo "and you have backup files before installing Arch Linux."
+echo "This script will install Arch Linux for you, and you can choose to install"
+echo "the desktop environment or not."
+echo "However, this script is NOT a replacement for the official installation guide,"
+echo "and you should read the official installation guide before using this script."
+echo "The official installation guide is here: "
+echo "https://wiki.archlinux.org/index.php/installation_guide" 
+echo "This script is NOT part of the official installation guide,"
+echo "use this script at your own risk."
 echo "$GREEN""-----------------------------------------------------------------------------""$RESET"
 echo "$GREEN""-----------------------------------------------------------------------------""$RESET"
 
