52a3df94fac13a35b160de1734d74f30f9beaeee
Markson <Markson@2333ma.top>
2023-06-16 11:28:14 +0800

    Use meta package instead of group

--- bin/setup-plasma.sh
+++ bin/setup-plasma.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-pacstrap /mnt plasma-meta plasma-wayland-session falkon vlc pipewire-pulse pipewire-jack plasma ksystemlog kcolorchooser colord-kde gwenview dolphin kcalc spectacle konsole noto-fonts-cjk noto-fonts-emoji noto-fonts-extra noto-fonts
+pacstrap /mnt plasma-meta kde-system-meta kde-graphics-meta kde-utilities-meta plasma-wayland-session falkon pipewire-pulse pipewire-jack noto-fonts-cjk noto-fonts-emoji noto-fonts-extra noto-fonts
 
 arch-chroot /mnt /bin/bash -c "systemctl enable sddm.service"
 arch-chroot /mnt /bin/bash -c "systemctl enable NetworkManager.service"
