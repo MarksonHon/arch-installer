7d1400099a4330c7fcf88a56c636d41810bf11a4
Markson <markson@2333ma.top>
2023-07-06 14:44:47 +0800

    Use zsh as default shell

--- bin/setup-hyprland.sh
+++ bin/setup-hyprland.sh
@@ -9,3 +9,4 @@ arch-chroot /mnt /bin/bash -c "systemctl enable NetworkManager.service"
 arch-chroot /mnt /bin/bash -c "systemctl enable bluetooth.service"
 
 cp ./customs/wayland.sh /mnt/etc/profile.d/wayland.sh
+cp ./customs/xdg-open /mnt/usr/local/bin/xdg-open
