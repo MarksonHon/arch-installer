1dca9649a01b67f2544ae69d9d80f0b6755f2763
Markson <markson@2333ma.top>
2023-07-30 19:13:46 +0800

    1

--- customs/wayland.sh
+++ customs/wayland.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
     export MOZ_ENABLE_WAYLAND=1
+    if command -v fcitx || command -v fcitx5; then
+        export XMODIFIERS=@im=fcitx
+    fi
 fi
