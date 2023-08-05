be19aabd6c4a93292d71a94c1d6c9df299e28d36
Markson <markson@2333ma.top>
2023-08-05 13:29:46 +0800

    Add cutsoms

--- custom-config.sh
+++ custom-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ! [ $EUID = 0 ]; then
+if [ $EUID = 0 ]; then
     echo "This script cannot be run as root!"
     echo "Please run this script as a normal user that can run as a sudoer."
     exit 1
@@ -31,8 +31,8 @@ setup_zsh(){
     sudo pacman -S pkgfile --noconfirm
     sudo pkgfile --update
     paru -S --noconfirm oh-my-posh-bin
-    paru -S --noconfirm setup-easy-zsh
-    echo '' > ~/.zshrc
+    paru -S --noconfirm easy-zsh-config
+    [ -f ~/.zshrc ] || cp customs/zshrc ~/.zshrc
     chsh -s /bin/zsh
 }
 
@@ -41,8 +41,13 @@ setup_fcitx5(){
     sudo cp customs/fcitx5.sh /etc/profile.d/
 }
 
+setup_pkgfile_hook(){
+    paru -S --noconfirm pacman-pkgfile-hook
+}
+
 setup_paru
 setup_zsh
+setup_pkgfile_hook
 setup_fcitx5
 [ -d ~/.config/fontconfig/ ] || mkdir -p ~/.config/fontconfig/
 [ -f ~/.config/fontconfig/fonts.conf ] || cp customs/fonts.conf ~/.config/fontconfig/
