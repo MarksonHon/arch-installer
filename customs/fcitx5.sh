98b9ef5d3d050f7180c181a774365f823293517e
Markson Hon <markson@localhost.local>
2024-10-21 19:27:18 +0800

    better fcitx5 config

--- customs/fcitx5.sh
+++ customs/fcitx5.sh
@@ -1,9 +1,11 @@
 # See more in 
 # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
 # https://fcitx-im.org/wiki/Setup_Fcitx_5
+# Set environment variables for fcitx5
 if command -v fcitx5 > /dev/null; then
     if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
         export XMODIFIERS=@im=fcitx5
+        export QT_IM_MODULES="wayland;fcitx;ibus"
         if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
             export GTK_IM_MODULE=fcitx5
             export QT_IM_MODULE=fcitx5
@@ -13,4 +15,17 @@ if command -v fcitx5 > /dev/null; then
         export QT_IM_MODULE=fcitx5
         export XMODIFIERS=@im=fcitx5
     fi
+fi
+# GTK settings
+if ! grep "\[Settings\]" < ~/.config/gtk-3.0/settings.ini > /dev/null; then
+    echo "[Settings]" >> ~/.config/gtk-3.0/settings.ini
+fi
+if ! grep "gtk-im-module=fcitx" < ~/.config/gtk-3.0/settings.ini > /dev/null; then
+    echo "gtk-im-module=fcitx" >> ~/.config/gtk-3.0/settings.ini
+fi
+if ! grep "\[Settings\]" < ~/.config/gtk-4.0/settings.ini > /dev/null; then
+    echo "[Settings]" >> ~/.config/gtk-4.0/settings.ini
+fi
+if ! grep "gtk-im-module=fcitx" < ~/.config/gtk-4.0/settings.ini > /dev/null; then
+    echo "gtk-im-module=fcitx" >> ~/.config/gtk-4.0/settings.ini
 fi
